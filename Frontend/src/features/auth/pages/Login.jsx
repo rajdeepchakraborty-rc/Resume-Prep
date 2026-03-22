f419a3b1fa3833e5427d43917460985e235b0439
Rajdeep Chakraborty <rajdeepchka@gmail.com>
2026-03-22 21:54:15 +0530

    Implement Styling and Navigation for login and registration forms

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import { useNavigate,Link } from 'react-router';
+import '../auth.form.scss'
 
 const Login = () => {
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
   return (
-    <div>
-      Login
-    </div>
+    <main>
+        <div className="form-container">
+            <h1>Login</h1>
+
+            <form onSubmit={handleSubmit}>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" placeholder='Enter email address'/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder='Enter password'/>
+                </div>
+
+                <button className="button primary-button">Login</button>
+            </form>
+
+            <p>Don't have an account? <Link to={"/register"}>Register</Link> </p>
+        </div>
+    </main>
   )
 }
 
